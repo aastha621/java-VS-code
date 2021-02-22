d746b85022af411a50dcc31f42bdbdc3b3b9bd8f
Aastha Jain <aasthajain2001.aj@gmail.com>
2021-02-22 12:11:56 +0530

    vs code java codes

---
+++ Main.java
@@ -0,0 +1,15 @@
+
+
+import java.util.*;
+
+class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        HashMap<Integer, Integer> hm= new HashMap<Integer, Integer>();
+
+        hm.put(1, 2);
+        System.out.println(hm.get(1));
+    }
+    
+}
