131addd91cd470ca5526e481044df845917025df
Aastha Jain <aasthajain2001.aj@gmail.com>
2021-03-25 22:19:43 +0530

    commit on master

---
+++ RedundantBrackets.java
@@ -0,0 +1,43 @@
+import java.util.*;
+public class RedundantBrackets {
+
+    public static boolean redundant(String s){
+        Stack<Character> st= new Stack<>();
+        
+         for(int i=0; i<s.length(); i++){
+            char c= s.charAt(i);
+
+            if(c==')')
+            {
+                if(st.peek()=='(')
+                return true;
+
+                else{
+                    boolean flag=true;
+
+                    while(st.peek()!='('){
+                        char top= st.pop();
+
+                        if(top=='+')
+                        flag=false;
+                    }
+
+                    if(flag==true)return true;
+
+                }
+
+                
+
+            }
+            else st.push(c);
+
+        }
+
+
+        return false;
+    }
+    public static void main(String args[]) {
+        System.out.println(redundant("(a+(b+g)+c)"));
+    }
+    
+}
