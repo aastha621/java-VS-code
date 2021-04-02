bc31db40d1c3c91f349c0d5b5d7c16f6f1fe673c
Aastha Jain <aasthajain2001.aj@gmail.com>
2021-04-02 20:02:56 +0530

    Graphs

---
+++ Graphs/traversals.java
@@ -0,0 +1,152 @@
+package Graphs;
+
+import java.util.ArrayList;
+
+
+public class traversals {
+
+    public static class Edge{
+        int v;
+        int w;
+
+        Edge(int a, int b){
+            v=a;
+            w=b;
+        }
+        
+
+    } 
+    
+    //A graph is collection of connected edges
+    // static ArrayList<Edge>[] graph= new ArrayList<>[8];
+
+    public static ArrayList<ArrayList<Edge>> graph= new ArrayList<>();
+
+
+    public static void addEdge(ArrayList<ArrayList<Edge>> graph, int u, int v, int w) 
+    {
+        graph.get(u).add( new Edge(v, w));
+        graph.get(v).add( new Edge(u, w));
+    }
+
+    public static boolean hasPath(ArrayList<ArrayList<Edge>> gr, int src, int des, boolean[] visited)
+    {
+        if(src==des)return true;
+
+        ArrayList<Edge> adjEdges= gr.get(src);
+        visited[src]=true;
+
+        for(Edge edge: adjEdges){
+            int nbr=edge.v;
+
+            if(!visited[nbr]){
+                boolean nbrPath=hasPath(gr, nbr, des, visited);
+
+                if(nbrPath)return true;
+            }
+
+        }
+
+        return false;
+
+    }
+
+    public static void DFS(int src, boolean[] vis)
+    //time complexity-> O(V+E)
+    //space complexity-> O(V) {for visited boolean array}
+    {
+        System.out.print(src+" ");
+
+        vis[src]=true;
+
+        ArrayList<Edge> nbrs= graph.get(src);
+
+        for(Edge e: nbrs)
+        {
+            int nbr= e.v;
+
+            if(!vis[nbr])
+            {
+                DFS(nbr, vis);
+            }
+        }
+
+    }
+
+
+    public static void printAllPaths(int src, int des, boolean[] vis, String psf) //time complexity?
+    {
+        if(src==des)
+        {
+            System.out.println(psf);
+            return;
+        }
+
+        vis[src]=true;
+        
+        ArrayList<Edge> adj = graph.get(src);
+
+        for(Edge e:adj)
+        {
+            int nbr=e.v;
+
+            if(!vis[nbr])
+            {
+                printAllPaths(nbr, des, vis, psf+nbr);
+            }
+        }
+
+        vis[src]=false;
+
+
+
+    }
+
+   
+
+    public static void main(String[] args) {
+
+        // ArrayList<Edge>[] graph= new ArrayList [6]; this representation is giving errors
+
+        // graph[0].add(new Edge(1, 2));
+
+        // ArrayList<ArrayList<Edge>> graph= new ArrayList<>();
+
+        for(int i=0; i<6; i++){
+            graph.add(i,new ArrayList<Edge>());
+        }
+
+
+        addEdge(graph, 0, 1, 1);
+        addEdge(graph, 1, 2, 1);
+        addEdge(graph, 2, 3, 1);
+        addEdge(graph, 3, 4, 1);
+        addEdge(graph, 4, 5, 1);
+        addEdge(graph, 5, 3, 1);
+        
+
+        boolean[] visited= new boolean[6];
+
+        System.out.println(hasPath(graph, 0, 2, visited));
+
+        DFS(0,visited);
+
+        printAllPaths(0, 5, visited, 0+"");
+
+
+
+        
+        
+        
+        
+
+        
+    }
+
+    
+
+    
+
+
+    
+}
--- Main.java
+++ Main.java
@@ -6,7 +6,7 @@ class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello, World.");
-        HashMap<Integer, Integer> hm= new HashMap<Integer, Integer>();
+        Map<Integer, Integer> hm= new HashMap<>();
 
         hm.put(1, 2);
         System.out.println(hm.get(1));
