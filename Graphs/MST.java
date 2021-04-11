2b0572a4bdf9f4775b0c0bc854210d1fd5cd1207
Aastha Jain <aasthajain2001.aj@gmail.com>
2021-04-11 20:52:39 +0530

    prims

---
+++ Graphs/MST.java
@@ -0,0 +1,121 @@
+package Graphs;
+import java.util.*;
+import Graphs.traversals.Edge;
+
+public class MST 
+{
+    //making a class that will store the certex value, parent and wt for each vertex
+    public static class graphNode implements Comparable<graphNode>
+    {
+        int ver;
+        int parent;
+        int key;
+
+        @Override
+        public int compareTo(graphNode o) {
+            
+            return this.key-o.key;
+        }
+
+        graphNode(int v,int p,  int k)
+        {
+            ver=v;
+            parent=p;
+            key=k;
+        }
+
+    }
+    
+    public static void PrimsDFS(ArrayList<ArrayList<Edge>> graph)
+    {
+        int v=graph.size();
+
+        boolean[] visited= new boolean[v];
+
+        PriorityQueue <graphNode> pq= new PriorityQueue<>();
+
+        //adding src node to pq
+        pq.add(new graphNode(0,-1,0));
+        System.out.println("The Minimum spanning tree for this graph is");
+
+        System.out.println("EDGE" +"    "+ "WEIGHT" );
+
+        while(!pq.isEmpty()){
+            graphNode node= pq.poll();
+
+            int vertex=node.ver;
+
+            if(visited[vertex])
+            continue;
+
+            visited[vertex]=true;
+
+            ArrayList<Edge> nbrs= graph.get(vertex);
+
+            if(node.parent!=-1)
+            System.out.println(node.parent+ " -> " + vertex +"    "+ node.key );
+
+
+            for(Edge edge: nbrs)
+            {
+                int nbr=edge.v;
+                int wt=edge.w;
+
+                if(!visited[nbr])
+                {
+                    
+                    pq.add(new graphNode(nbr,node.ver, wt));
+                }
+            }
+
+
+        }
+
+
+
+
+
+        
+
+
+
+    }
+    public static void main(String[] args) {
+        ArrayList<ArrayList<Edge>> graph= new ArrayList<>();
+
+        for(int i=0; i<9; i++){
+            graph.add(i,new ArrayList<Edge>());
+        }
+
+        traversals.addEdge(graph, 0, 1, 4);
+        traversals.addEdge(graph, 0, 7, 8);
+        traversals.addEdge(graph, 1, 2, 8);
+        traversals.addEdge(graph, 1, 7, 11);
+        traversals.addEdge(graph, 2, 3, 7);
+        traversals.addEdge(graph, 2, 8, 2);
+        traversals.addEdge(graph, 2, 5, 4);
+        traversals.addEdge(graph, 3, 4, 9);
+        traversals.addEdge(graph, 3, 5, 14);
+        traversals.addEdge(graph, 4, 5, 10);
+        traversals.addEdge(graph, 5, 6, 2);
+        traversals.addEdge(graph, 6, 7, 1);
+        traversals.addEdge(graph, 6, 8, 6);
+        traversals.addEdge(graph, 7, 8, 7);
+
+
+        // traversals.addEdge(graph, 0, 1, 1);
+        // traversals.addEdge(graph, 1, 2, 1);
+        // traversals.addEdge(graph, 2, 3, 1);
+        // traversals.addEdge(graph, 3, 4, 1);
+        // traversals.addEdge(graph, 4, 5, 1);
+        // traversals.addEdge(graph, 5, 3, 1);
+
+
+        PrimsDFS(graph);
+        
+    }
+
+
+
+    
+}
---
+++ sorting.java
@@ -0,0 +1,94 @@
+import java.util.Arrays;
+
+public class sorting {
+
+    public static void swap(int[] arr, int i, int j){
+
+        int temp=arr[j];
+        arr[j]=arr[i];
+        arr[i]=temp;
+
+    }
+
+    public static  int[] bubbleSort(int[] arr) //for ascending
+    {
+        /*
+        number of swaps for an element= number of elements on right side which are smaller than it.
+        Total swaps= sum of count of swaps for each element!
+        */
+
+        for(int i=0; i<arr.length-1; i++){
+            for(int j=1; j<arr.length-i; j++)
+            {
+               if(arr[j-1]>arr[j])
+               swap(arr, j-1, j);
+
+            }
+
+        }
+
+        return arr;
+
+    }
+
+   public static int swaps=0;
+
+    public static int[] insertionSort (int[] arr){
+
+
+        for(int i=0; i<arr.length-1; i++){
+            for(int j=i+1; j>0; j--)
+            {
+                if(arr[j]<arr[j-1])
+                {
+                    swap(arr, j, j-1);
+                    swaps++;
+                }
+            }
+
+        }
+
+        return arr;
+    }
+
+    public static int[] selectionSort(int[] arr)
+    {
+
+        for(int i=0; i<arr.length-1; i++){
+            int minIndex=i;
+
+            for(int j=i+1; j<arr.length; j++)
+            {
+                if(arr[j]<arr[minIndex])
+                {
+                    minIndex=j;
+                }
+
+            }
+
+            swap(arr, i, minIndex);
+        }
+
+        return arr;
+
+    }
+
+
+
+
+    public static void main(String[] args) {
+        int[] arr= {3,1,4,2,9,5,6};
+
+       int[] sorted= selectionSort(arr);
+
+       for(int x:sorted){
+           System.out.print(x+" ");
+       }
+
+
+
+    }
+
+
+    
+}
