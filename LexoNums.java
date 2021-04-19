311351f826501d977ca21f9bfb50a58d08b38e7d
Aastha Jain <aasthajain2001.aj@gmail.com>
2021-04-19 20:07:40 +0530

    commit

--- LexoNums.java
+++ LexoNums.java
@@ -1,24 +1,27 @@
-public class LexoNums {
+public class LexoNums{
 
     public static void lexoOrder(int i , int n){
-        if(i>n)
-        return;
-    
-    System.out.println(i);
-    
-    for(int j=0; j<10; j++)
-    {
-        lexoOrder(i*10+j, n);
-    }
+            if(i>n)
+            return;
         
-    }
-
-    public static void main(String[] args) {
-
-        int n=13;
-        for(int i=1; i<=n; i++){
-           lexoOrder(i, n);
+        System.out.println(i);
+        
+        for(int j=0; j<10; j++)
+        {
+            lexoOrder(i*10+j, n);
         }
-    }
+            
+        }
+    
+        public static void main(String[] args) {
+    
+            int n=13;
+            for(int i=1; i<=n; i++){
+               lexoOrder(i, n);
+            }
+        }
+        
+    
+    
     
 }
